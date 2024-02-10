f1fcf2f577bc6799397c46087a7d7b8af948e178
Achintya Choudhari <122711576+AchintyaCh@users.noreply.github.com>
2024-02-10 11:17:12 +0530

    Update signin.js

--- signin.js
+++ signin.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   // Render your HTML form here
-  res.sendFile(__dirname + '/signin.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/storeNumber', (req, res) => {

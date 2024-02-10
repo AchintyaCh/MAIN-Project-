f8e002e63f4aa7aed2e19f68cde39021e6353d37
Achintya Choudhari <122711576+AchintyaCh@users.noreply.github.com>
2024-02-10 11:11:50 +0530

    Create signin.js

---
+++ signin.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000; // Update the port if needed
+
+const pool = mysql.createPool({
+  host: 'setuplib.c90g2owkymp9.ap-south-1.rds.amazonaws.com', // Replace with your RDS endpoint
+  user: 'admin',     // Replace with your MySQL username
+  password: 'admin123', // Replace with your MySQL password
+  database: 'user', // Replace with your MySQL database name
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+// Middleware for parsing JSON and url-encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  // Render your HTML form here
+  res.sendFile(__dirname + '/signin.html');
+});
+
+app.post('/storeNumber', (req, res) => {
+  const userInput = req.body.mail;
+  const userInput2 = req.body.username;
+  const userInput3 = req.body.password;
+
+
+  // Insert the data into the database
+  pool.query('INSERT INTO user2 (`Mail Id`,Username,Password) VALUES (?,?,?)', [userInput,userInput2,userInput3], (error, results) => {
+    if (error) {
+      return res.status(500).send('Error inserting data into the database.');
+    }
+    
+
+    res.send('Data successfully inserted into the database.');
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
